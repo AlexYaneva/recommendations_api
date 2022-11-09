3a31fae478eaa72cf06d25654eaf9dcf5aa44b36
Alex Yaneva <alexandrayaneva@yahoo.com>
2022-11-09 16:27:09 +0000

    .

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// http methods
-// app.get();
-// app.post();
-// app.put();
-// app.delete();
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -21,6 +15,6 @@ app.get('/api/series/:seriesid', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Recommendations API listening on port ${port}`)
 })
 
