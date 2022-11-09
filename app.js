bb5967d9c3fff9d2f0dc662b807f9c09a1e923d5
Alex Yaneva <alexandrayaneva@yahoo.com>
2022-11-09 16:25:00 +0000

    initial routes

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// http methods
-// app.get();
-// app.post();
-// app.put();
-// app.delete();
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
